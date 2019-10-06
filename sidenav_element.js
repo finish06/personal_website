72eec90089141154b30f5740fe90f8fd2be8194e
Caleb Dunn <finish.06@gmail.com>
2019-10-06 14:42:21 -0500

    update name to sidebar

--- sidenav_element.js
+++ sidenav_element.js
@@ -1,21 +1,29 @@
 import { LitElement, html, css } from 'lit-element';
 import '/bio.js'
 
-export class SideNavElement extends LitElement {
+export class SidebarElement extends LitElement {
     static get styles() {
         return css`
-            .sidenav {
-                height: 100%;
-                width: 15%;
-                position: fixed;
-                z-index: 1;
-                top: 0;
-                left: 0;
-                background-color: #2d3c49;
-                overflow-x: hidden;
-                padding-top: 80px;
+            @media only screen and (min-width: 1025px) {
+                .sidebar {
+                    height: 100%;
+                    width: 325px;
+                    position: fixed;
+                    z-index: 1;
+                    top: 0;
+                    left: 0;
+                    background-color: #2d3c49;
+                    overflow-x: hidden;
+                }
+                h1 {
+                    font-size: 2.9rem;
+                    margin-bottom: 0;
+                }
+                h2 {
+                    font-size: 1.5rem;
+                }
             }
-            .sidenav a {
+            .sidebar a {
                 text-align: center;
                 padding-top: 8px;
                 padding-bottom: 8px;
@@ -26,18 +34,23 @@ export class SideNavElement extends LitElement {
               }
               #home {
                   padding-top: 16px;
+              }
+              h1, h2 {
+                color: #7d97ad;
+                text-align: center;
               }`
     }
 
     render() {
         return html`
-        <div class="sidenav">
+        <div class="sidebar">
+            <div id="name">
+                <h1 class="introduction" id="calebdunn">CALEB DUNN</h1>
+                <h2 class="introduction">Life Long Learner</h2>
+            </div>
             <bio-element></bio-element>
-            <a id="home" href="/index.html">Home</a>
-            <a href="/resume/resume.pdf">Resume</a>
-            <a href="/certificates/certificates.html">Certificates</a>
         </div>`;
     }
 }
 
-customElements.define('sidenav-element', SideNavElement)
+customElements.define('sidebar-element', SidebarElement)
