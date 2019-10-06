d4749274b76da2ce61436236f88a88be5af89a01
Caleb Dunn <finish.06@gmail.com>
2019-10-06 11:32:36 -0500

    new sidenav component

---
+++ sidenav_element.js
@@ -0,0 +1,43 @@
+import { LitElement, html, css } from 'lit-element';
+
+export class SideNavElement extends LitElement {
+    static get styles() {
+        return css`
+            .sidenav {
+                height: 100%;
+                width: 200px;
+                position: fixed;
+                z-index: 1;
+                top: 0;
+                left: 0;
+                background-color: #2d3c49;
+                overflow-x: hidden;
+                padding-top: 80px;
+            }
+            .sidenav a {
+                padding: 6px 6px 6px 32px;
+                text-decoration: none;
+                font-size: 1.5rem;
+                color: #7d97ad;
+                display: block;
+              }
+              img {
+                display: block;
+                margin: auto;
+                border-radius: 35%;
+                max-width: 85%;
+              }`
+    }
+
+    render() {
+        return html`
+        <div class="sidenav">
+            <img class="pictures face" id="face_large" src="images/professionalPhoto.jpg" alt="Image of Caleb">
+            <a href="index.html">Home</a>
+            <a href="resume/resume.pdf">Resume</a>
+            <a href="certificates/certificates.html">Certificates</a>
+        </div>`;
+    }
+}
+
+customElements.define('sidenav-element', SideNavElement)
